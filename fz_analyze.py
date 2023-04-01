aa92fdf715be881c0225e1df53a9d511c5a228ea
katie_minji <katiemk1@uci.edu>
2023-03-31 19:08:21 -0700

    plot instead of scatter
    to see the relationship for each mice better. Differenciate better

--- fz_analyze.py
+++ fz_analyze.py
@@ -43,7 +43,6 @@ with open("master.pkl", "rb") as f:
     
 master_dict = pickle.loads(rawdata)
         
-
 #%%
 
 # extract only fz and dictionary of {mouse: [[pxl shift by index],[],[]]}
@@ -65,6 +64,7 @@ def after(my_dict):
     return indexed
 
 indexed = after(master_dict)
+
 
 # # between
 # def between(my_dict):
@@ -248,25 +248,15 @@ def plotting(day_type, dictionary, title):
 
     from matplotlib import pyplot as plt
 
-    plt.style.use('seaborn')
-    if len(day_type) == 4:
-        fig, axs = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=[10,4.8])
-        axs = axs.ravel()
-    elif len(day_type) == 2:
-        fig = plt.figure()
-    fig.suptitle(title,fontsize=15)
-    fig.tight_layout()
-        
     i = 0
     for day, by_day in dictionary.items():  #loop through D1, D2....
         max_length, average = my_max_length(by_day)  #max presentation, average for each presentation
         
         if day == day_type[-1]:  #if context B
             fig = plt.figure()
-            plt.plot(average,linewidth=2.5, color='#7f7f7f')
             over = 0
             for name, individual in by_day.items():    
-                plt.scatter(range(len(individual)),individual,s=17, label=name)
+                plt.plot(range(len(individual)),individual, label=name)
                 if len(individual) < max_length:    
                     plt.vlines(x=len(individual),ymin=-3,ymax=105, colors='#bf80ff')
                     over += 1
@@ -284,27 +274,7 @@ def plotting(day_type, dictionary, title):
             plt.tight_layout(pad=1.5)
         
         else:   #if context A
-            if len(day_type) == 4:
-                axs[i].plot(average,linewidth=2.5, color='#7f7f7f')
-                for name, individual in by_day.items():    
-                    axs[i].scatter(range(len(individual)),individual,s=17, label=name)
-                axs[i].set_title(day)
-                axs[i].set_ylim([-3, 105])
-                axs[i].set_xticks(range(0, max_length, 1))
-                axs[i].set_xlabel('Presentation #',fontsize=13)
-                axs[0].set_ylabel('Freezing Percentage',fontsize=13)
-                axs[2].legend(bbox_to_anchor=(1.0, 1.0),fontsize=12)
-                plt.tight_layout()
-            elif len(day_type) == 2:
-                plt.plot(average,linewidth=2.5, color='#7f7f7f')
-                for name, individual in by_day.items():    
-                    plt.scatter(range(len(individual)),individual,s=17, label=name)
-                plt.ylim([-3, 105])
-                plt.xticks(range(0, max_length, 1))
-                plt.xlabel('Presentation #',fontsize=13)
-                plt.ylabel('Freezing Percentage',fontsize=13)
-                plt.legend(bbox_to_anchor=(1.3, 1.0),fontsize=12)
-                plt.tight_layout()
+            pass
         i+=1
         
 
@@ -312,7 +282,7 @@ def plotting(day_type, dictionary, title):
 graphs = []
 for con in reduced_dict:
     info = reduced_dict[con]['info']
-    for condition in ['FS','ctr']:
+    for condition in ['FS']:
         day_type, title = day_type_and_title(info, condition)
         my_con = reduced_dict[con][condition]  #mouse dict within same condition and (FS/ctr)
         if bool(my_con) == False:   #if we don't have data, dictionary empty
@@ -325,230 +295,3 @@ for con in reduced_dict:
 print('')
 print(graphs)  #print data we have
 
-            
-            
-#%%
-
-
-# group graphing
-
-
-# group into specific conditions
-
-# groupby = ['10s']  #MD,SD,1s,10s,15x,8x
-# groupby = ['con1','con2','con3','con4','con5','con6','con7','con8']
-# groupby = ['con1','con5','con6']
-# groupby = ['con5','con6']
-groupby = ['con5']
-
-
-def my_filter(groupby, reduced_dict):
-    
-    filtered = {}
-    for con in groupby:
-        
-        if con in reduced_dict.keys():  #for groupby ['con1','con2']
-            filtered[con] = reduced_dict[con] 
-        
-        for condition, value in reduced_dict.items():  #loop through con1,con2 of reduced_dict
-            
-            have = True
-            for x in groupby:  #for each condition(e.g. 'MD')
-                if x in value['info']:  #for broupby ['MD', '1s']
-                    pass
-                else:
-                    have = False  #if go against at least one of condition, no
-                    
-            if have == True:
-                filtered[condition] = reduced_dict[condition] 
-
-    return filtered
-
-
-def legend_day_type(filtered):
-    
-    md = False
-    for con in filtered.values():
-        if con['info'][0] == 'MD':
-            md = True
-        #for legend
-        day = con['info'][0]
-        tone = con['info'][1]
-        cycle = con['info'][2]
-        legend = f'{day}, {tone}, {cycle}x'
-        con['legend'] = legend
-    
-    if md == True:
-        day_type = ['D1', 'D2', 'D3', 'D4']
-    else:  #if single day
-        day_type = ['D1', 'D2']
-    
-    return day_type, filtered
-
-
-def organize_to_dict(filtered):
-    dictionary = {'FS':{}, 'ctr':{}}
-    for condition in dictionary.keys():  #among FS and ctr
-        for value in filtered.values():
-            legend = value['legend']
-            data = value[condition]  #data for condition (FS/ctr)
-            dictionary[condition].update({legend: data})
-    return dictionary
-
-
-    
-def imax_length(fz):
-    
-    maxl = []
-    for x in fz:  #for list of perc in day[mouse]
-        maxl.append(len(x))
-    maxl = max(maxl)
-    # average by index
-    average = []
-    for i in range(maxl):
-        hi = []
-        for mouse in fz:
-            try:  #try to append by each index, until max length
-                hi.append(mouse[i])
-            except:
-                pass
-        average.append(sum(hi)/len(hi))  #average for each presentation
-        
-    return average
-
-
-def to_average(v,day_type):
-    
-    avg_day = {}  #make copy of v, no empty dict, Day directly instead of mouse
-    for legend in v:
-        print(f'{legend}: {len(v[legend])}')
-        avg_day.update({legend: {}})
-        for day in day_type:
-            avg_day[legend].update({day: {}})
-    
-    for legend, data in v.items():
-        for day in day_type:
-            fz = []
-            for mouse, dfs in data.items():
-                if day in dfs:
-                    fz_data = dfs[day]['Freeze(%)']
-                fz.append(fz_data)
-            average = imax_length(fz)
-            avg_day[legend][day] = average
-    
-    return avg_day
-        
-
-def filter_average(average):
-    
-    mod_average = {}
-    
-    for legend, data in average.items():
-        if 'MD' in legend:
-            mod_average[legend] = data
-        elif len(data) == 4:  #if single day is not 2 days, instead 4
-            mod_data = {}
-            mod_data['D1'] = data['D1']
-            mod_data['D2'] = data['D2']
-            mod_data['D3'] = data['D2']
-            mod_data['D4'] = data['D2']
-            mod_average[legend] = mod_data
-        else:  #if single day is 2 days
-            mod_average[legend] = data
-
-    return mod_average
-    
-
-
-def plotting_average(average, day_type, con, groupby):
-    
-    from matplotlib import pyplot as plt
-
-    plt.style.use('seaborn')
-    title = f'Grouped({con}): {groupby}'
-    
-    if len(day_type) == 4:
-        fig, axs = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=[10,4.8])
-        axs = axs.ravel()
-    elif len(day_type) == 2:
-        fig = plt.figure()
-        
-    fig.suptitle(title,fontsize=15)
-    fig.tight_layout()
-    
-    i = 0
-    for day in day_type:
-        
-        if day == day_type[-1]:  #if context B
-            fig = plt.figure()
-            maxl = []
-            for legend, data in average.items():
-                plt.plot(data[day], label=legend, linewidth=2.5)
-                maxl.append(len(data[day]))
-                
-            plt.title(f'{title}: context B',fontsize=15)
-            plt.ylim([-3,105])
-            plt.xticks(range(0, max(maxl), 1))
-            plt.xlabel('Presentation #',fontsize=13)
-            plt.ylabel('Freezing Percentage',fontsize=13)
-            plt.legend(bbox_to_anchor=(1.3, 1.0),fontsize=12)
-            plt.tight_layout(pad=1.5)
-        
-        else:   #if context A
-            if len(day_type) == 4:
-                maxl = []
-                for legend, data in average.items():
-                    if ((legend[0] == 'S') and (day == 'D1')) or (legend[0] == 'M'):  #if single day and first day
-                        axs[i].plot(data[day], label=legend, linewidth=2.5)
-                        maxl.append(len(data[day]))
-                axs[i].set_title(day)
-                axs[i].set_ylim([-3, 105])
-                axs[i].set_xticks(range(0, max(maxl), 1))
-                axs[i].set_xlabel('Presentation #',fontsize=13)
-                axs[0].set_ylabel('Freezing Percentage',fontsize=13)
-                axs[0].legend(loc='upper left',bbox_to_anchor=(-0.8,1.0),fontsize=12)
-                plt.tight_layout()
-                
-            elif len(day_type) == 2:
-                maxl = []
-                for legend, data in average.items():
-                    plt.plot(data[day], label=legend, linewidth=2.5)
-                    maxl.append(len(data[day]))
-                plt.ylim([-3, 105])
-                plt.xticks(range(0, max(maxl), 1))
-                plt.xlabel('Presentation #',fontsize=13)
-                plt.ylabel('Freezing Percentage',fontsize=13)
-                plt.legend(bbox_to_anchor=(1.3, 1.0),fontsize=12)
-                plt.tight_layout()
-        i+=1
-    
-    
-filtered = my_filter(groupby,reduced_dict)
-day_type, filtered = legend_day_type(filtered)
-dictionary = organize_to_dict(filtered)
-
-        
-for con,v in dictionary.items():  #for FS/ctr and dict of {MD,1s,8x:{mice}}
-    print(con)
-    iv = {}
-    for legend, data in v.items():
-        if bool(data) == False:
-            pass
-        else:
-            iv[legend] = data
-    average = to_average(iv, day_type)
-    average_mod = filter_average(average)
-    plotting_average(average_mod, day_type, con, groupby)
-    print('')
-    
-        
-        
-
-
-#%%
-
-# delete useless variables
-
-del auto_dict, average, con, condition, data, day_type, dictionary
-del f, filtered, graphs, groupby, indexed, info, iv, keys, legend, my_con, my_time
-del rawdata, title, v
