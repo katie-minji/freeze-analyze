62a70e476c7a287c6066301bfc7a72a3101d750f
katie-minji <katiemk1@uci.edu>
2023-04-17 17:10:52 -0700

    updated data function for new ctxB
    (thresh 30, under 100 considered 0)

--- fz_analyze.py
+++ fz_analyze.py
@@ -123,6 +123,32 @@ def fz_to_percentage(auto_dict):
     return my_dict
 
 
+def data(numbers):
+    boolean, x = [], []
+    for idx, num in enumerate(numbers):
+        x.append(num)
+        if idx%3 == 0:
+            counter = len([i for i in x if i<100])
+            thresh = len([i for i in x if i>100])
+            if (counter >= 2) and (thresh == 0):  #if 2 or more out of 3 are 0 --> fz
+                boolean.append(1)
+            else:  #if less or equal to 1 out of 3 are 0 --> not fz
+                boolean.append(0)
+            x = []
+    total, counter = 0, 0
+    for value in boolean:
+        if value == 1:  #if freezing is true
+            counter += 1
+        elif value == 0:
+            if counter >= 5:  #if more than 5 in a row is freezing
+                total += counter*3  #3 frames per fz or not fz value
+            counter = 0
+    total += counter*3  #add up last values too 
+    return total / len(numbers) * 100
+
+data(hi)
+
+
 
 # extract one of (freeze_only,area_list, timestamps) from final_dict
 def extract_fz_only(my_input, my_type):
